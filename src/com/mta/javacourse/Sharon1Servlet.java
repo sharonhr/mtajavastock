d485b5fe20a2b0d60956c65825cdafcc1ac55481
sharonhr <sharon@Sharon-PC>
2014-11-02 17:35:58 +0200

    Initial commit.

---
+++ src/com/mta/javacourse/Sharon1Servlet.java
@@ -0,0 +1,13 @@
+package com.mta.javacourse;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Sharon1Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, Sharon!");
+	}
+}
