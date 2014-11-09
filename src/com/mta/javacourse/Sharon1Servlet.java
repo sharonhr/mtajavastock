5910373dd345bb92a57a287302955877584eeaee
sharonhr <sharon@Sharon-PC>
2014-11-09 16:54:39 +0200

    1

--- src/com/mta/javacourse/Sharon1Servlet.java
+++ src/com/mta/javacourse/Sharon1Servlet.java
@@ -7,7 +7,15 @@ import javax.servlet.http.*;
 public class Sharon1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, Sharon!");
+		resp.setContentType("text/html");
+		int num1, num2, num3;
+		num1=3;
+		num2=4; 
+		num3=7;
+		
+		int result = ((3+4)*7);
+		String resultStr =new String("Result of " + "(" + num1 + "+" + num2 + ")" + "*" +num3 + "=" +result);
+		
+		resp.getWriter().println(resultStr);
 	}
 }
