1cfc92220b5f87056eadc9092262fcd10f9dea06
sharonhr <sharon@Sharon-PC>
2014-11-17 08:28:51 +0200

    EX3

--- src/com/mta/javacourse/Sharon1Servlet.java
+++ src/com/mta/javacourse/Sharon1Servlet.java
@@ -1,6 +1,7 @@
 package com.mta.javacourse;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -8,14 +9,38 @@ public class Sharon1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/html");
+		
+		//ex1
 		int num1, num2, num3;
 		num1=3;
 		num2=4; 
 		num3=7;
+		int result = ((num1+num2)*num3);
 		
-		int result = ((3+4)*7);
 		String resultStr =new String("Result of " + "(" + num1 + "+" + num2 + ")" + "*" +num3 + "=" +result);
 		
-		resp.getWriter().println(resultStr);
+		//EX2
+		
+		//part1
+		double result2;
+		double radius;
+		radius = 50;
+		result2 = Math.pow(radius, 2) * Math.PI;
+		String result2Str = new String("Area of circle with radius"+radius+" is "+result2+" square CM");
+		
+		//part2
+		double oppositeLength;
+		double angleB= 30;
+		int hypotenuse= 50;
+		angleB= Math.toRadians(angleB);
+		oppositeLength= Math.sin(angleB)*hypotenuse;
+		String result3Str= new String("Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is: "+oppositeLength+" cm");
+		
+		//part3
+		double powerResult= Math.pow(20, 13);
+		String result4Str=new String("Power of 20 with exp of 13 is: "+powerResult );
+		
+		String FinalStr= new String(resultStr + "<br>" + result2Str + "<br>" + result3Str + "<br>" + result4Str);
+		resp.getWriter().println(FinalStr);
 	}
 }
